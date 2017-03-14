44efdd28641c270f90b29ccc584fb723e3a6e4fb
sadhakbj <sadhakbj@gmail.com>
2017-03-14 16:13:33 +0545

    MSG01 Initial Setup

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,49 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
+  <div id="app">
+    <div class="container">
+      <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+          <div class="panel panel-default">
+            <div class="panel-heading">List of all the pages you manage.</div>
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
+            <div class="panel-body">
+              @{{ message }}
+              <button @click="updateMe">Update me</button>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
 @endsection
+
+
+<script src="https://unpkg.com/vue@2.2.2"></script>
+<script type="text/javascript">
+  window.onload = function() {
+
+    var vue = new Vue({
+      el: '#app',
+      data: {
+        message: 'Welcome To the application from vue.',
+        showMe: false,
+      },
+      methods: {
+        updateMe() {
+          alert('hello world');
+        },
+        listAllThePages() {
+          $.ajax({
+
+          });
+        }
+      },
+      mounted() {
+        let self = this;
+        self.listAllThePages();
+      }
+    });
+  };
+</script>
